c1e794ba87b57925c851f9034b1807d4e6b213b0
Ivan Lambrev <ilambrev@gmail.com>
2026-08-15 15:43:23 +0300

    Day 4 part 2 solution

---
+++ day_04/day_04_part_2.py
@@ -0,0 +1,20 @@
+import hashlib
+
+file_path = "day_04_sample_data.txt"
+
+file = open(file_path, "r")
+key = file.read()
+file.close()
+
+i = 0
+
+while True:
+    s = f"{key}{i}"
+    res = hashlib.md5(s.encode())
+
+    if res.hexdigest()[0:6] == "000000":
+        break
+    else:
+        i += 1
+        
+print(i)
